1001fd139ccd9a400e834d61d7eb1a645cea9791
CEINMARK <CEINMARK@DESKTOP-4GR91FV>
2021-03-24 11:44:58 +0100

    hola marta

---
+++ src/EXAMEN/EXAMEN_24M_PRIMO.java
@@ -0,0 +1,20 @@
+package EXAMEN;
+
+import java.util.Scanner;
+
+public class EXAMEN_24M_PRIMO {
+	
+	public static void main(String arg[]) {
+		
+		System.out.println("Buenos días Ceinmark, soy Iván Marín");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce un numero");
+		int num=sc.nextInt();
+
+			
+	}
+
+
+}
